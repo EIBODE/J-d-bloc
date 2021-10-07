89dd1092db27616afc49943b33ca7ddf6fb9204b
Henrique Dos Santos Martins <henrique0126@gmail.com>
2021-10-07 15:14:21 +0200

    'up'

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://eibode.github.io/J-d-bloc/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://mentos126.github.io/my-kyhoot/' : '/',
   pluginOptions: {
     i18n: {
       locale: 'fr',
